4f014920d981881ff2a02a608b51b8d988b333e5
Your Name <you@example.com>
2020-02-01 04:25:13 +0800

    mysql连接

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,9 @@ from app.user import init_view
 
 def create_app():
     app = Flask(__name__)
-    #虚拟机数据库你还能把我怎么的
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymsqyl:///root:js@192.168.116.132:3306/test'
+    # 虚拟机数据库你还能把我怎么的
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:js@192.168.116.132:3306/test'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
     # 执行先后顺序无关
     init_view(app=app)

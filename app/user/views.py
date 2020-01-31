143cca766fde4fe431ebb41fbca7bc28fd82a7f3
Your Name <you@example.com>
2020-02-01 05:09:41 +0800

    路由属性，url_for生成url

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for
 from flask import render_template
 
 from app.user.models import models, User
@@ -8,24 +8,54 @@ user_bp = Blueprint('user', __name__)
 
 @user_bp.route('/')
 def hello_world():
-    #Flask有个template_folder属性，默认是app所在脚本的templates目录，render_template会去这个位置找index.html
-    #可以通过设置template_folder属性修改templates位置，static_folder同理
-    return render_template('user/index.html',hello='hello,world')
+    # Flask有个template_folder属性，默认是app所在脚本的templates目录，render_template会去这个位置找index.html
+    # 可以通过设置template_folder属性修改templates位置，static_folder同理
+    return render_template('user/index.html', hello='hello,world')
+
 
 @user_bp.route('/create/')
 def create_model():
     models.create_all()
     return '创建成功'
 
+
 @user_bp.route('/adduser')
 def add_user():
-    user=User()
-    user.nickname='asdfaf'
-    #自定义save方法
+    user = User()
+    user.nickname = 'asdfaf'
+    # 自定义save方法
     user.save()
     return '创建用户成功'
+
 
 @user_bp.route('/dropdb')
 def drop_all():
     models.drop_all()
     return '删库，跑路'
+
+
+# 默认字符串不匹配/
+# @user_bp.route('/name/<name>')
+# @user_bp.route('/name/<int:name>')
+@user_bp.route('/name/<path:name>')
+def get_name(name):
+    print(type(name))
+    return name
+
+
+# 加数字不行
+# 多个路由对应一个视图函数
+# 还有一个uuid不写了
+@user_bp.route('/any/<any(a,werqwer):xxxx>/')
+def get_any(xxxx):
+    print(type(xxxx))
+    # 重定向到hello_world视图
+    # return 'hello' + str(xxxx)
+
+    # return redirect('/')
+    # 根据视图函数的endpoint生成url,endpoint默认为函数名字，可以通过endpoint属性修改
+    # return redirect(url_for('user.hello_world'))
+
+    #还可以传参数
+    #蓝图名字.endpoint
+    return redirect(url_for('user.get_name',name='a'))
--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 from flask_script import Manager, Server
 from app import app
 
+from flask_migrate import MigrateCommand
+
 manager = Manager(app=app)
 manager.add_command("runserver", Server(use_debugger=True))
-
+manager.add_command("db", MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()

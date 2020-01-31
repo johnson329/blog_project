feb4b009bd7f9d889795ba5bf28983ea408574e2
Your Name <you@example.com>
2020-02-01 02:37:46 +0800

    使用模板

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
-
+from flask import render_template
 user_bp = Blueprint('user', __name__)
 
 
 @user_bp.route('/')
 def hello_world():
-    return 'hello world!'
+    #Flask有个template_folder属性，默认是app所在脚本的templates目录，render_template会去这个位置找index.html
+    #可以通过设置template_folder属性修改templates位置，static_folder同理
+    return render_template('user/index.html',hello='hello,world')
